d15c6e89c2075eb3a27bd9bfefa1833859b479a8
josescalia <josescalia@gmail.com>
2018-01-17 11:31:39 +0700

    Implementing Personal Github Token

--- js/vue/app/github_ajax_service.js
+++ js/vue/app/github_ajax_service.js
@@ -3,14 +3,27 @@ new Vue({
     data: {
         gitHubApiUrl: "https://api.github.com", //default github api url
         gitHubUserName: "josescalia", //default is me
-
+        gitHubToken : "YOUR_GITHUB_PERSONAL_ TOKEN",
+        reqHeader:{},
+        loading: true,
         //github object api
         gitHubUserInfo: {},
         gitHubUserRepo: {},
-        gitHubUserRepoList:[],
+        gitHubUserRepoList: [],
+
+        //pagination
+        dataPerPage: 10,
+        currPage: 1,
+        next: 0,
+        prev: 0,
+        numOfPage: 0,
+        totalData: 0
     },
-    mounted:function(){
-         this.getGitHubUserInfo();
+    mounted: function () {
+        let authorization = "token " + this.gitHubToken;
+        this.reqHeader = {headers: {"Authorization":authorization}};
+        this.getGitHubUserInfo();
+
     },
     methods: {
         //utility
@@ -20,26 +33,47 @@ new Vue({
 
         /*getGithub UserFunction*/
         getGitHubUserInfo: function () {
+            $("#modalLoading").modal("show");
             let gitHubUrl = this.gitHubApiUrl + "/users/" + this.gitHubUserName;
-            this.$http.get(gitHubUrl).then(response => {
-                this.gitHubUserInfo = response.body;
-                this.gitHubUserInfo.created_at = this.formatDate(this.gitHubUserInfo.created_at);
-                this.gitHubUserInfo.updated_at = this.formatDate(this.gitHubUserInfo.updated_at);
-                //if success get repo list of user
-                this.getGitHubRepoList();
-                },response =>{
+            this.$http.get(gitHubUrl,this.reqHeader).then(response => {
+                    this.gitHubUserInfo = response.body;
+                    this.gitHubUserInfo.created_at = this.formatDate(this.gitHubUserInfo.created_at);
+                    this.gitHubUserInfo.updated_at = this.formatDate(this.gitHubUserInfo.updated_at);
+                    //if success get repo list of user
+                    this.getGitHubRepoList(this.currPage);
+                    this.totalData = this.gitHubUserInfo.public_repos;
+                    this.numOfPage = this.getNumPage();
+
+                }, response => {
                     //error callback
                     alert("The user is " + response.body.message + " on github");
+                    $("#modalLoading").modal("hide");
                 }
             );
         },
 
-        getGitHubRepoList:function () {
-            let gitHubUrl = this.gitHubApiUrl + "/users/" + this.gitHubUserName + "/repos";
-            this.$http.get(gitHubUrl).then(response => {
+        getGitHubRepoList: function (page) {
+            $("#modalLoading").modal("show");
+            let gitHubUrl = this.gitHubApiUrl + "/users/" + this.gitHubUserName + "/repos?per_page=" + this.dataPerPage + "&page=" + page;
+            this.$http.get(gitHubUrl,this.reqHeader).then(response => {
                 this.gitHubUserRepoList = response.body;
+                $("#modalLoading").modal("hide");
+                this.createPaging(page);
+
             });
+        },
+
+
+        createPaging: function (page) {
+            this.currPage = page;
+            this.prev = page -1;
+            this.next = page + 1;
+        },
+
+        getNumPage: function () {
+            return Math.ceil(this.gitHubUserInfo.public_repos / this.dataPerPage);
         }
+
 
     }
 });
